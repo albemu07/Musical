32399ef32dd428d6ee3557419544ceb7e7f5fadb
Alberto <albemu07@ucm.es>
2023-02-21 19:20:01 +0100

    ejercicio9

---
+++ entrega/ejercicio9.py
@@ -0,0 +1,77 @@
+import numpy as np         # arrays    
+import sounddevice as sd   # modulo de conexión con portAudio
+import soundfile as sf     # para lectura/escritura de wavs
+import kbhit               # para lectura de teclas no bloqueante
+
+CHUNK = 1024
+
+filename = 'piano.wav'
+C, samplerate = sf.read(filename)
+pitch_table = {'C': 1, 'D': 9/8, 'E': 5/4, 'F': 4/3, 'G': 3/2, 'A': 5/3, 'B': 15/8, 
+    'c':2, 'd': 9/4, 'e': 5/2, 'f': 8/3, 'g': 3, 'a': 10/3, 'b': 15/4,}
+
+def addDelay(sound):
+    delay = 0.2
+    zeros = np.zeros(np.int64(delay * samplerate))
+    soundDelayed = np.float32(np.concatenate((zeros, sound), axis=0))
+    return soundDelayed
+
+# Función para obtener el array de muestras correspondiente a una nota
+def get_note_array(note_name):
+    # Interpolar el array de muestras para obtener la nota correspondiente
+    #if frequency > samplerate:
+    xToInt = np.arange(0, len(C), pitch_table[note_name])
+    x = np.arange(0, len(C), 1)
+    note_array = np.interp(xToInt, x, C)
+    
+    return np.float32(note_array)
+
+def main():
+
+    # abrimos stream de salida
+    stream = sd.OutputStream(
+    samplerate = samplerate,            # frec muestreo 
+    blocksize  = CHUNK,            # tamaño del bloque (muy recomendable unificarlo en todo el programa)
+    channels   = 1)  # num de canales
+
+    # arrancamos stream
+    stream.start()
+    kb = kbhit.KBHit()
+    c= ' '
+
+    vol = 1.0
+    print('\n\nProcessing chunks: ',end='')
+
+    # termina con 'q' o cuando no queden samples
+    end = False # será true cuando el chunk esté vacio
+
+    i = 0
+
+    buff = []
+
+    while c!= 'k' and not(end)>0: 
+        # modificación de volumen 
+        if kb.kbhit():
+            note = " "
+            c = kb.getch()
+            other = np.concatenate((get_note_array('C'), np.zeros(np.int64(0.2 * samplerate))))
+            buff = np.float32(np.sum((other, addDelay(get_note_array('C')))))
+            #print("Vol: ",vol) 
+
+        # lo pasamos al stream
+        if (len(buff) != 0):
+            stream.write(buff[:CHUNK] * vol) # escribimos al stream
+            buff = buff[CHUNK:]
+            if(len(buff) == 0):
+                buff = []
+            elif(len(buff) < CHUNK):
+                zeros = CHUNK - len(buff)
+                buff = np.float32(np.concatenate((buff, np.zeros(zeros)), axis=0))
+
+    print('end')
+
+    stream.stop()
+    stream.close()
+    kb.set_normal_term()
+
+main()
